b77f3fd6dfce3eb3f77569721cbc6adc88dd36b4
mckellardw <dwm269@cornell.edu>
2022-03-02 14:14:36 -0500

    - 10x_kallisto updates - Added util to Seutils to get spatial coordinates for Visium data, based on cell barcode and whitelist

--- align_pipes/10x_kallisto/scripts/kb.sh
+++ align_pipes/10x_kallisto/scripts/kb.sh
@@ -22,8 +22,18 @@ mkdir -p ${OUTDIR}
 cd ${OUTDIR}
 touch ${LOG}
 
+# Add params to log file
+echo "~~ Parameters ~~~" >> ${LOG}
+echo "Output directory: " ${OUTDIR}  >> ${LOG}
+echo "Index used: " ${KB_IDX} >> ${LOG}
+echo "Whitelist used: " ${WHITELIST} >> ${LOG}
+echo "Chemistry: " ${CHEMISTRY} >> ${LOG}
+echo "Barcode/UMI read file: " ${R1FQ} >> ${LOG}
+echo "RNA read file:" ${R2FQ} >> ${LOG}
+echo " " >> ${LOG}
+
 # Pseudoalign and generate .bus file
-echo "~~~Pseudoaligning with `kallisto bus`...\n\n " >> ${LOG}
+echo "~~~Pseudoaligning with `kallisto bus`... " >> ${LOG}
 kallisto bus \
 -i ${KB_IDX} \
 -x ${CHEMISTRY} \
@@ -32,24 +42,43 @@ kallisto bus \
 -t ${THREADS} \
 --verbose \
 ${R1FQ} ${R2FQ} 2>> ${LOG}
+echo " " >> ${LOG}
+
+
+# Correct cell/spot barcodes
+echo "~~~Correcting barcodes... " >> ${LOG}
+bustools correct \
+--whitelist ${WHITELIST} \
+-o output.sorted.bus \
+output.bus 2>> ${LOG}
+echo " " >> ${LOG}
 
 # Sort .bus file
-echo "~~~Sorting output bus...\n\n " >> ${LOG}
+echo "~~~Sorting output bus... " >> ${LOG}
 bustools sort \
 -t ${THREADS} \
 -m ${MEMLIMIT} \
--o output.sorted.bus \
-output.bus 2>> ${LOG}
-
-# Correct cell/spot barcodes
-echo "~~~Correcting barcodes...\n\n " >> ${LOG}
-bustools correct \
---whitelist ${WHITELIST} \
 -o output.corrected.bus \
 output.sorted.bus 2>> ${LOG}
+echo " " >> ${LOG}
+
+# Inspect outputs
+echo "~~~Inspecting sorted/corrected BUS file..." >> ${LOG}
+bustools inspect \
+--whitelist ${WHITELIST} \
+--ecmap matrix.ec \
+output.corrected.bus 2>> ${LOG}
+
+echo "~~~Writing to inspect.corrected.bus.json" >> ${LOG}
+bustools inspect \
+--whitelist ${WHITELIST} \
+--ecmap matrix.ec \
+--output inspect.corrected.bus.json \
+output.corrected.bus
+echo "Done!" >> ${LOG}
 
 # Convert bus file to text for easier counting
-echo "~~~Converting bus to text...\n\n " >> ${LOG}
-bustools text \
--o output.corrected.txt \
-output.corrected.bus
+# echo "~~~Converting bus to text... " >> ${LOG}
+# bustools text \
+# -o output.corrected.txt \
+# output.corrected.bus
